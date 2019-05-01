e3893251c38358651e4cae42377db99c76c46fe3
Menelik Tefera <menelikworku@gmail.com>
2019-05-01 09:53:19 -0400

    Deleted history file and userd BrowserRouter in App.js

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,12 +1,11 @@
 import React from "react";
-import { Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import { connect } from "react-redux";
 import * as actions from "../actions";
 import Header from "./Header";
 import Landing from "./Landing";
 import Dashboard from "./Dashboard";
 import SurveyNew from "./surveys/SurveyNew";
-import history from "../history";
 
 class App extends React.Component {
 	componentDidMount() {
@@ -16,7 +15,7 @@ class App extends React.Component {
 	render() {
 		return (
 			<div>
-				<Router history={history}>
+				<BrowserRouter>
 					<div>
 						<Header />
 						<div className="container">
@@ -39,7 +38,7 @@ class App extends React.Component {
 							</Switch>
 						</div>
 					</div>
-				</Router>
+				</BrowserRouter>
 			</div>
 		);
 	}
